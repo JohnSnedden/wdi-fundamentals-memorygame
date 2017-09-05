5ff5fe5e9c6fd8d62bb076757b0a27396daf2f97
John Snedden <john.snedden@libertymutual.com>
2017-09-05 10:54:07 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// initialize card values
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+// output selected card values to console
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
