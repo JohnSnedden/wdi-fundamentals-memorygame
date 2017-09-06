a0e76058f06dad697364a637a0f33ebf59fabbcb
John Snedden <john.snedden@libertymutual.com>
2017-09-06 15:01:19 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 
-// initialize card values
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// create card arrays
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {  // check to see if two cards have been chosen
+  if (cardsInPlay[0] === cardsInPlay[1]) {  // check if  the two cards match
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
 
 // output selected card values to console
 console.log("User flipped " + cardOne);
